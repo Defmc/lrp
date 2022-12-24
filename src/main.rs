8b6e965313e68aee4b02a26cabedb2cc0647d6f6
Defmc <defmc0101@gmail.com>
2022-12-23 23:24:56 -0300

    feat: impl `closure`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::iter::Extend;
 
 macro_rules! rule {
     ($grammar:tt, $rule:literal -> $($($terms:literal)*)|*) => {
@@ -58,40 +59,57 @@ pub type Grammar = HashMap<&'static str, Vec<Vec<&'static str>>>;
 /// A = . . . T B -> {T: FIRST(B)}
 /// A = . . . . T -> {T: FOLLOW(A)}
 pub type Table = HashMap<&'static str, HashSet<&'static str>>;
-pub type State = HashSet<Position>;
 
 /// terminal sets
 pub type TermSet = HashSet<&'static str>;
 
-#[derive(Debug, Clone)]
+pub type State = HashMap<&'static str, HashSet<Position>>;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Position {
     pub stack: Vec<&'static str>,
-    pub idx: usize,
+    pub top: isize,
+    pub lookahead: Vec<&'static str>,
+}
+
+impl Position {
+    pub fn new(stack: Vec<&'static str>, top: isize, lookahead: Vec<&'static str>) -> Self {
+        Self {
+            stack,
+            top,
+            lookahead,
+        }
+    }
+
+    pub fn adv(&mut self) {
+        self.top += 1;
+    }
+
+    pub fn peek(&self, idx: isize) -> Option<&'static str> {
+        let idx: usize = (self.top + idx).try_into().ok()?;
+        self.stack.get(idx).copied()
+    }
+
+    pub fn locus(&self) -> Option<&'static str> {
+        self.peek(1)
+    }
+
+    pub fn top(&self) -> Option<&'static str> {
+        self.peek(0)
+    }
 }
 
 impl From<Vec<&'static str>> for Position {
     fn from(value: Vec<&'static str>) -> Self {
-        Position {
+        Self {
             stack: value,
-            idx: 0,
+            top: -1,
+            lookahead: Vec::new(),
         }
     }
 }
 
-impl Position {
-    pub fn adv(&mut self) {
-        self.idx += 1;
-    }
-
-    pub fn locus(&self) -> Option<&'static str> {
-        self.stack.get(self.idx + 1).copied()
-    }
-
-    pub fn top(&self) -> Option<&'static str> {
-        self.stack.get(self.idx).copied()
-    }
-}
-
+#[derive(Debug, Default)]
 pub struct Parser {
     pub grammar: Grammar,
     pub terminals: TermSet,
@@ -102,22 +120,24 @@ pub struct Parser {
 impl Parser {
     #[must_use]
     pub fn new(grammar: Grammar, terminals: TermSet) -> Self {
-        let first = Self::gen_first(&grammar);
-        let follow = Self::gen_follow(&grammar, &terminals);
-        Self {
+        let mut buf = Self {
             grammar,
             terminals,
-            first,
-            follow,
-        }
+            ..Default::default()
+        };
+        buf.first = dbg!(buf.gen_first());
+        buf.proc_first();
+        buf.follow = dbg!(buf.gen_follow());
+        buf.proc_follow();
+        buf
     }
 
     /// # Panics
     /// Never.
     #[must_use]
-    pub fn gen_first(grammar: &Grammar) -> Table {
+    pub fn gen_first(&self) -> Table {
         let mut table = Table::new();
-        for (name, rules) in grammar {
+        for (name, rules) in &self.grammar {
             table.insert(name, HashSet::new());
             for rule in rules.iter().filter(|r| &r[0] != name) {
                 table.get_mut(name).unwrap().insert(rule[0]);
@@ -129,20 +149,25 @@ impl Parser {
     /// # Panics
     /// Never.
     #[must_use]
-    pub fn gen_follow(grammar: &Grammar, terminals: &TermSet) -> Table {
+    pub fn gen_follow(&self) -> Table {
         let mut table = Table::new();
-        for (name, rules) in grammar {
+        for (name, rules) in &self.grammar {
             for rule in rules {
                 for term_idx in 0..rule.len() - 1 {
-                    if !terminals.contains(rule[term_idx]) {
-                        table
-                            .entry(rule[term_idx])
-                            .or_insert_with(HashSet::new)
-                            .insert(rule[term_idx + 1]);
+                    if !self.is_terminal(rule[term_idx]) {
+                        let entry = table.entry(rule[term_idx]).or_insert_with(HashSet::new);
+                        if self.is_terminal(rule[term_idx + 1]) {
+                            // A = . . . T a -> {T: a}
+                            entry.insert(rule[term_idx + 1]);
+                        } else {
+                            // A = . . . T B -> {T: FIRST(B)}
+                            entry.extend(self.first[rule[term_idx + 1]].clone())
+                        }
                     }
                 }
                 let last = rule.last().unwrap();
-                if !terminals.contains(last) {
+                if !self.is_terminal(last) {
+                    // A = . . . . T -> {T: FOLLOW(A)}
                     table.entry(last).or_insert_with(HashSet::new).insert(name);
                 }
             }
@@ -198,6 +223,48 @@ impl Parser {
     pub fn is_terminal(&self, term: &str) -> bool {
         self.terminals.contains(term)
     }
+
+    #[must_use]
+    pub fn state(&self, rule: &'static str) -> State {
+        let mut set = HashSet::new();
+        for choice in &self.grammar[rule] {
+            set.insert(Position::from(choice.clone()));
+        }
+        State::from([(rule, set)])
+    }
+
+    #[must_use]
+    pub fn gen_closure(&self, state: State) -> State {
+        let mut new_state = State::new();
+        for (name, poss) in state.into_iter() {
+            new_state.insert(name, poss.clone());
+            for mut pos in poss.into_iter() {
+                while let Some(locus) = pos.locus() {
+                    if !self.is_terminal(locus) {
+                        let lookahead: Vec<&str> = if let Some(next) = pos.peek(2) {
+                            if self.is_terminal(next) {
+                                vec![next]
+                            } else {
+                                self.first[next].iter().copied().collect()
+                            }
+                        } else {
+                            self.follow[locus].iter().copied().collect()
+                        };
+                        let poss: HashSet<_> = self.grammar[locus]
+                            .iter()
+                            .map(|r: &Vec<&str>| Position::new(r.clone(), -1, lookahead.clone()))
+                            .collect();
+                        new_state
+                            .entry(locus)
+                            .or_insert_with(HashSet::new)
+                            .extend(poss);
+                    }
+                    pos.adv();
+                }
+            }
+        }
+        new_state
+    }
 }
 
 fn main() {
@@ -208,11 +275,9 @@ fn main() {
        Add -> Factor
        Factor -> Factor * Term
        Factor -> Term
-       Term -> Expr
-       Term -> Lvalue
-       Expr -> ( Add )
-       Lvalue -> int
-       Lvalue -> ident
+       Term -> ( Add )
+       Term -> int
+       Term -> ident
     */
     let grammar = grammar! {
         "Start" -> "Add",
@@ -220,21 +285,24 @@ fn main() {
             | "Factor",
         "Factor" -> "Factor" "*" "Term"
             | "Term",
-        "Term" -> "Expr" | "Lvalue",
-        "Expr" -> "(" "Add" ")",
-        "Lvalue" -> "int" | "ident"
+        "Term" -> "(" "Add" ")" | "int" | "ident"
     };
-    let terminals = HashSet::from(["int", "ident", "(", "*", "+", ")"]);
+    let terminals = HashSet::from(["int", "ident", "(", "*", "+", ")", "$", "a", "b", "d", "x"]);
 
     println!("grammar: {grammar:?}");
     println!("terminals: {terminals:?}\n");
 
-    let mut parser = Parser::new(grammar, terminals);
-    println!("first-step FIRST table: {:?}", parser.first);
-    parser.proc_first();
-    println!("final FIRST table: {:?}\n", parser.first);
+    let parser = Parser::new(grammar, terminals.clone());
 
-    println!("first-step FOLLOW table: {:?}", parser.follow);
-    parser.proc_follow();
-    println!("final FOLLOW talbe: {:?}", parser.follow);
+    println!("FIRST table: {:?}", parser.first);
+    println!("FOLLOW table: {:?}", parser.follow);
+
+    let test_g = grammar! {
+        "S" -> "a" "A" "b" "$"
+            | "a" "B" "d" "$",
+        "A" -> "x",
+        "B" -> "x"
+    };
+    let test = Parser::new(test_g, terminals);
+    println!("closure {:?}", test.gen_closure(test.state("S")));
 }
