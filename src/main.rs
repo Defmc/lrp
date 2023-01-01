013bf74aeb1eb50279e90795d77049b671e9f58a
Defmc <defmc0101@gmail.com>
2023-01-01 12:15:51 -0300

    feat: impl a frontend to parser data

---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use lrp::{Clr, Grammar, Map, Parser, Set, Tabler};
+use prettytable::{row, Cell, Row, Table};
+
+fn main() {
+    let grammar = lrp::grammar! {
+        "S" -> "d"
+    };
+
+    let grammar = Grammar::new("S", grammar, Set::from(["c", "d"]));
+    let mut parser = Clr::new(grammar);
+    print_tokens_table(parser.tables());
+
+    parser.proc_closures();
+    parser.proc_actions();
+
+    print_states_table(parser.tables());
+    print_actions_table(parser.tables());
+}
+
+fn print_tokens_table(table: &Tabler) {
+    let mut out = Table::new();
+    out.set_titles(row!["Non terminal", "First tokens", "Follow tokens"]);
+
+    let iter = table.first.iter().zip(table.follow.iter().map(|(_, f)| f));
+
+    for ((k, first), follow) in iter {
+        out.add_row(row![k, format!("{first:?}"), format!("{follow:?}")]);
+    }
+
+    out.printstd();
+}
+
+fn print_states_table(table: &Tabler) {
+    let mut out = Table::new();
+    out.set_titles(row!["Goto(Idx, Term)", "Kernel", "State", "Closure"]);
+
+    let internal = table.basis_pos();
+    let start = &table.states[0];
+
+    out.add_row(row!["", format!("{internal:?}"), "0", format!("{start:?}")]);
+
+    let mut idx = 0;
+    let mut virt_len = 0;
+    let mut history = Map::new();
+    while idx < table.states.len() {
+        let row = table.states[idx].clone();
+        for s in table.grammar.symbols() {
+            let kernel = Tabler::sym_filter(&row, &s);
+            if kernel.is_empty() {
+                continue;
+            }
+            let state_idx = if !history.contains_key(&kernel) {
+                history.insert(kernel.clone(), virt_len + 1);
+                virt_len += 1;
+                virt_len
+            } else {
+                history[&kernel]
+            };
+            out.add_row(row![
+                format!("goto({idx}, {s})"),
+                format!("{kernel:?}"),
+                format!("{state_idx}"),
+                format!("{:?}", table.states[table.kernels[&kernel]])
+            ]);
+        }
+        idx += 1;
+    }
+
+    out.printstd();
+}
+
+fn print_actions_table(table: &Tabler) {
+    let mut out = Table::new();
+
+    let (terminals, nonterminals) = table
+        .grammar
+        .symbols()
+        .chain(["LRP'START"].into_iter())
+        .fold((Set::new(), Set::new()), |(mut ts, mut nts), s| {
+            if table.grammar.is_terminal(&s) {
+                ts.insert(s);
+            } else {
+                nts.insert(s);
+            }
+            (ts, nts)
+        });
+
+    let rows: Vec<_> = terminals
+        .iter()
+        .chain(["State"].iter())
+        .chain(nonterminals.iter())
+        .map(|t| Cell::new(t))
+        .collect();
+
+    out.set_titles(Row::new(rows));
+
+    let mut row_buf = Vec::new();
+    for (state_idx, state) in table.actions.iter().enumerate() {
+        row_buf.clear();
+        for t in &terminals {
+            let item = if let Some(act) = state.get(t) {
+                format!("{act:?}")
+            } else {
+                String::new()
+            };
+            row_buf.push(Cell::new(&item));
+        }
+        row_buf.push(Cell::new(&format!("{state_idx}")));
+
+        for nt in &nonterminals {
+            let item = if let Some(act) = state.get(nt) {
+                format!("{act:?}")
+            } else {
+                String::new()
+            };
+            row_buf.push(Cell::new(&item));
+        }
+
+        out.add_row(Row::new(row_buf.clone()));
+    }
+
+    out.printstd();
+}
