055a1267046d90aa7ca93978dd2c078d4ab5b36c
Defmc <defmc0101@gmail.com>
2023-01-07 17:33:07 -0300

    feat: add DFA benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,23 +1,31 @@
 use std::{fmt, time::Duration};
 
-use hermes_bench::{BenchSize, Bencher, ClassicBench};
-use lrp::{Clr, Grammar, Lalr, Parser, Tabler};
+use hermes_bench::{BenchSize, Bencher, ClassicBench, IterBench};
+use lrp::{dfa::Error, Clr, Dfa, Grammar, Lalr, Parser, Tabler};
 
 mod grammars;
 
-const BENCH_SIZE: BenchSize = BenchSize::Time(Duration::from_secs(1));
+const BENCH_SIZE: BenchSize = BenchSize::Iters(100);
 
-const GRAMMARS: &[(fn() -> Grammar, &'static str)] = &[
-    (grammars::dragon_book, "dragon's book"),
-    (grammars::serokell, "serokell"),
-    (grammars::ucalgary_uni_oth_lr1, "ucalgary_uni_oth_lr1"),
-    (grammars::wikipedia, "wikipedia"),
-    (grammars::puncs, "punctuations"),
+const GRAMMARS: &[(fn() -> Grammar, &[&[&str]], &'static str)] = &[
+    (
+        grammars::dragon_book,
+        grammars::DRAGON_BOOK_INPUTS,
+        "dragon's book",
+    ),
+    (grammars::serokell, grammars::SEROKELL_INPUTS, "serokell"),
+    (
+        grammars::ucalgary_uni_oth_lr1,
+        grammars::UCALGARY_UNI_OTH_LR1_INPUTS,
+        "ucalgary_uni_oth_lr1",
+    ),
+    (grammars::wikipedia, grammars::WIKIPEDIA_INPUTS, "wikipedia"),
+    (grammars::puncs, grammars::PUNCS_INPUTS, "punctuations"),
 ];
 
 fn test_table_parser_prod<P: Parser + PartialEq + fmt::Debug>(name: &str) {
     println!("\n{name} productions:");
-    for (grammar, grammar_name) in GRAMMARS {
+    for (grammar, _, grammar_name) in GRAMMARS {
         let parser = P::new(grammar());
         let assert = |p| assert_eq!(p, parser);
         let table_copy = || Tabler::new(grammar());
@@ -32,7 +40,7 @@ fn test_table_parser_prod<P: Parser + PartialEq + fmt::Debug>(name: &str) {
 
 fn test_table_gen() {
     println!("\nTabler setup:");
-    for (grammar, grammar_name) in GRAMMARS {
+    for (grammar, _, grammar_name) in GRAMMARS {
         let table = Tabler::new(grammar());
         let assert = |p| assert_eq!(p, table);
         let grammar_copy = || grammar();
@@ -45,8 +53,29 @@ fn test_table_gen() {
     }
 }
 
+fn test_dfa<P: Parser>(name: &str) {
+    println!("\n{name}'s DFA:");
+    for (grammar, inputs, grammar_name) in GRAMMARS {
+        let parser = P::new(grammar());
+        let actions_copy = || parser.tables().actions.clone();
+        let iter = inputs.into_iter().cycle().map(|i| (i, actions_copy()));
+        let assert = |r| assert!(matches!(r, Ok(_) | Err(Error::Conflict(_, _))));
+        let mut bench = IterBench::new(iter, &|(input, table)| {
+            let mut dfa = Dfa::new(input.into_iter().copied(), table);
+            dfa.start()
+        })
+        .with_name(format!("{grammar_name} parsing inputs"))
+        .with_post(&assert)
+        .with_size(BENCH_SIZE);
+        bench.run();
+        println!("\t{bench}");
+    }
+}
+
 fn main() {
     test_table_gen();
     test_table_parser_prod::<Clr>("Canonical LR");
     test_table_parser_prod::<Lalr>("LALR(1)");
+    test_dfa::<Clr>("Canonical LR");
+    test_dfa::<Lalr>("LALR(1)");
 }
