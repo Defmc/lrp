464ed1c0560f22952f02d91961bf5d162e8804c1
Defmc <defmc0101@gmail.com>
2023-01-22 00:01:42 -0300

    fix: add grammar types

--- wop/src/lib.rs
+++ wop/src/lib.rs
@@ -3,11 +3,30 @@ use logos::Logos;
 #[derive(Logos, Debug, PartialEq)]
 pub enum Token {
     /// Specifies the grammar type. e.g:
-    /// ```
+    /// ``
     ///     grammar lr | lalr | slr;
-    /// ```
+    /// ``
     #[token("grammar")]
     Grammar,
+
+    /// LR(1) (Canonical LR parser) grammar type.
+    /// It's the more powerful, but uses a lot of memory more than LALR(1) or SLR.
+    /// As LR(k) can process all CFGs and every LR(k) can be converted to a LR(1) grammar, this
+    /// type can parse every CFG grammar.
+    #[token("lr")]
+    LrParser,
+
+    /// LALR(1) grammar type.
+    /// It's the ideal type for almost every grammar. Has a balance between power and memory usage.
+    /// Can take more compile-time than LR(1) grammars due to table optimizations and is guaranteed
+    /// to have the same SLR parser table size.
+    #[token("lalr")]
+    LalrParser,
+
+    /// SLR grammar type.
+    /// It's the simplest type and has the fatest compile-time.
+    #[token("slr")]
+    SlrParser,
 
     /// An identifier:
     /// ab A AB a__ a0219 _a1 _
@@ -15,9 +34,9 @@ pub enum Token {
     Ident,
 
     /// Specifies which type will be used as token. e.g:
-    /// ```
+    /// ``
     ///     link wop::Token;
-    /// ```
+    /// ``
     #[token("link")]
     Link,
 
@@ -31,60 +50,64 @@ pub enum Token {
     PathAccess,
 
     /// Declares a new rule. e.g:
-    /// ```
-    ///     rule TwoIdents = Ident Ident;
-    /// ```
+    /// ``
+    ///     TwoIdents = Ident Ident;
+    /// ``
     #[token("rule")]
     RuleDecl,
 
     /// A normal rule. e.g:
-    /// ```
-    ///     rule GrammarType = "grammar" ("lr" | "lalr" | "slr");
-    /// ```
+    /// ``
+    ///     GrammarType = "grammar" ("lr" | "lalr" | "slr");
+    /// ``
     /// It's the same of:
-    /// ```
-    ///     rule TermDecls = ("term" str "=" Ident ";")*;
-    /// ```
+    /// ``
+    ///     TermDecls = ("term" str "=" Ident ";")*;
+    /// ``
     #[token("=")]
     NormalSpec,
 
     /// A variadic specific rule. e.g:
-    /// ```
-    ///     rule TermDecls *= "term" str "=" Ident ";";
-    /// ```
+    /// ``
+    ///     TermDecls *= "term" str "=" Ident ";";
+    /// ``
     /// It's the same of:
-    /// ```
-    ///     rule TermDecls = ("term" str "=" Ident ";")*;
-    /// ```
+    /// ``
+    ///     TermDecls = ("term" str "=" Ident ";")*;
+    /// ``
     #[token("*=")]
     VarSpec,
 
     /// A repeated specific rule. e.g:
-    /// ```
-    ///     rule TermDecls += "term" str "=" Ident ";";
-    /// ```
+    /// ``
+    ///     TermDecls += "term" str "=" Ident ";";
+    /// ``
     /// It's the same of:
-    /// ```
-    ///     rule TermDecls = ("term" str "=" Ident ";")+;
-    /// ```
+    /// ``
+    ///     TermDecls = ("term" str "=" Ident ";")+;
+    /// ``
     #[token("+=")]
     RepSpec,
 
     /// An optional specific rule. e.g:
-    /// ```
-    ///     rule RuleType ?= "?" | "+" | "*";
-    /// ```
+    /// ``
+    ///     RuleType ?= "?" | "+" | "*";
+    /// ``
     /// It's the same of:
-    /// ```
-    ///     rule RuleType = ("?" | "+" | "*")?;
-    /// ```
+    /// ``
+    ///     RuleType = ("?" | "+" | "*")?;
+    /// ``
     #[token("?=")]
     OptSpec,
 
-    /// Type or ident specifier. Used to define the return type of a rule or a gramem variable name
+    /// Type or ident specifier. Used to define the return type of a or a gramem variable name
     /// for parsing expression.
     #[token(":")]
     Type,
+
+    /// String literal
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    StrLit,
 
     /// Metadata included attribute. Handles a gramem variable as a `lrp::Token`
     /// NOTE: Using it with a `~` attribute creates some order differences:
@@ -101,31 +124,77 @@ pub enum Token {
     BoxAttr,
 
     /// Or production clause. Allows to expand others productions to the same rule. e.g:
-    /// ```
-    ///     rule GrammarType = "grammar" ("lr" | "lalr" | "slr");
-    /// ```
+    /// ``
+    ///     GrammarType = "grammar" ("lr" | "lalr" | "slr");
+    /// ``
     #[token("|")]
     Or,
 
     /// Context guards. Allows to re-use a production fraction. e.g:
-    /// ```
-    ///     rule GrammarType = "grammar" ("lr" | "lalr" | "slr");
-    /// ```
+    /// ``
+    ///     GrammarType = "grammar" ("lr" | "lalr" | "slr");
+    /// ``
     #[token("(")]
     OpenParen,
     #[token(")")]
     CloseParen,
 
     /// Parsing expressions. A piece of Rust code to handle a reduction process. e.g:
-    /// ```
-    ///     rule Sum: i32 = Int:lhs "+" Int:rhs => { lhs + rhs }
-    /// ```
-    #[token("=>")]
+    /// ``
+    ///     Sum: i32 = Int:lhs "+" Int:rhs => lhs + rhs;;
+    /// ``
+    #[regex(r#"=>.*;;"#)]
     Pexpr,
 
     #[error]
     Error,
 
-    #[regex(r"[ \t\n\f]+", logos::skip)]
+    /// Whitespace
+    #[regex(r"[ \t\n\r]+", logos::skip)]
     Ws,
+
+    /// Block comment
+    #[regex(r#"/\*([^\*]|\*[^//])*\*/"#, logos::skip)]
+    BlockComment,
+
+    /// Line comment
+    #[regex(r#"//[^\n]*\r?"#, logos::skip)]
+    LineComment,
+}
+
+#[cfg(test)]
+mod tests {
+    use logos::Logos;
+
+    use crate::Token;
+    #[test]
+    fn strings() {
+        let mut toks = Token::lexer(
+            r#"
+            ""
+            "em"
+            "\"aaaada"
+            "é o crime perfeito\""
+            "\"ce ta sempre linda mulher\""
+            "\\\"\\\""
+            // Just a comment...
+            /* thats amazing */
+            /* a fake* final block comment */
+            NowAnIdent
+            "#,
+        );
+        let mut tokens = vec![
+            Token::Ident,
+            Token::StrLit,
+            Token::StrLit,
+            Token::StrLit,
+            Token::StrLit,
+            Token::StrLit,
+            Token::StrLit,
+        ];
+        while let Some(tok) = toks.next() {
+            println!("{tok:?}: {} | {:?}", toks.slice(), toks.span());
+            assert_eq!(Some(tok), tokens.pop());
+        }
+    }
 }
