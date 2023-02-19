e95c3e7d2ec4ca249654724f4c0aff858f936d58
Defmc <defmc0101@gmail.com>
2023-02-19 09:27:12 -0300

    fix: show tokens before the error

--- wop/src/main.rs
+++ wop/src/main.rs
@@ -7,13 +7,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = std::env::args().skip(1).next().unwrap();
     let file = fs::read_to_string(path)?;
 
-    let lexer = wop::Sym::lexer(&file).map(|m| Token::empty(m));
+    let mut copy = Vec::new();
+
+    let lexer = wop::Sym::lexer(&file).spanned().map(|(m, s)| {
+        copy.push(&file[s.clone()]);
+        println!("{m:?} | {}", &file[s]);
+        Token::empty(m)
+    });
     let slr = Slr::new(wop::grammar());
 
     let mut dfa = slr.simple_dfa(lexer);
 
     if let Err(e) = dfa.trace(|st| println!("{:?}", st.stack_fmt())) {
         println!("FATAL: {e}");
+        println!("source:");
+        copy.into_iter().for_each(|a| print!("{a} "));
     }
 
     Ok(())
