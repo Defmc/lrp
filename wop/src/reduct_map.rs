e894b51e23de92b588c37818ff1d5a382809fef0
Defmc <defmc0101@gmail.com>
2023-09-09 17:57:35 -0400

    feat(wop/parser): implement all missing reducers

--- wop/src/reduct_map.rs
+++ wop/src/reduct_map.rs
@@ -14,12 +14,22 @@ pub fn reduct_map() -> ReductMap<Meta<Ast>, Sym> {
 
     let mut map = ReductMap::new();
     map.insert(Sym::EntryPoint, vec![entry_point]);
-    map.insert(Sym::Program, vec![program_rec, program_rec]);
-    map.insert(Sym::Declaration, vec![decl, decl, decl]);
-    map.insert(Sym::TokenDecl, vec![tokendecl, tokendecl]);
-    map.insert(Sym::IdentPath, vec![identpath]);
-    map.insert(Sym::UseDecl, vec![usedecl]);
-    map.insert(Sym::AssignOp, vec![assignop]);
+    map.insert(Sym::Program, vec![program_rec; 2]);
+    map.insert(Sym::Declaration, vec![decl; 3]);
+    map.insert(Sym::TokenDecl, vec![token_decl; 2]);
+    map.insert(Sym::IdentPath, vec![ident_path; 2]);
+    map.insert(Sym::UseDecl, vec![use_decl]);
+    map.insert(Sym::AssignOp, vec![assign_op; 4]);
+    map.insert(Sym::AttrPrefix, vec![attr_prefix; 4]);
+    map.insert(Sym::AttrSuffix, vec![attr_suffix; 3]);
+    map.insert(Sym::VarPipe, vec![var_pipe]);
+    map.insert(Sym::TypeDecl, vec![type_decl]);
+    map.insert(Sym::ElmBase, vec![elm_base; 6]);
+    map.insert(Sym::Elm, vec![elm; 4]);
+    map.insert(Sym::Prod, vec![prod; 4]);
+    map.insert(Sym::RulePipeRepeater, vec![rule_pipe_repeater; 4]);
+    map.insert(Sym::RulePipe, vec![rule_pipe; 4]);
+    map.insert(Sym::RuleDecl, vec![rule_decl; 2]);
     map
 }
 
@@ -41,26 +51,109 @@ fn decl(decl: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
     Meta::new(Ast::Declaration(decl[0].clone().into()), span)
 }
 
-fn tokendecl(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+fn token_decl(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
     let token = toks[1].clone();
     let ident = toks[2].clone();
     let span = (token.item.start, ident.item.end);
     Meta::new(Ast::TokenDecl(token.into(), ident.into()), span)
 }
 
-fn identpath(path: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+fn ident_path(path: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
     let start = path.first().unwrap().item.start;
     let end = path.last().unwrap().item.end;
     let program = path.into_iter().cloned().collect();
-    Meta::new(Ast::Program(program), (start, end))
+    Meta::new(Ast::IdentPath(program), (start, end))
 }
 
-fn usedecl(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+fn use_decl(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
     let span = (toks[0].item.start, toks[1].item.end);
     Meta::new(Ast::UseDecl(toks[1].clone().into()), span)
 }
 
-fn assignop(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+fn assign_op(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
     let span = (toks[0].item.start, toks[0].item.end);
     Meta::new(Ast::AssignOp(toks[0].clone().into()), span)
+}
+
+fn attr_prefix(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks
+        .into_iter()
+        .map(|t| Meta::new(t.item.item.as_sym(), (t.item.start, t.item.end)))
+        .collect();
+    Meta::new(Ast::AttrPrefix(program), (start, end))
+}
+
+fn attr_suffix(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let span = (toks[0].item.start, toks[0].item.end);
+    Meta::new(
+        Ast::AttrSuffix(
+            toks[0]
+                .item /* TODO: couldn't it be `ty` directly */
+                .item
+                .as_sym(),
+        ),
+        span,
+    )
+}
+
+fn var_pipe(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let span = (toks[0].item.start, toks[0].item.end);
+    Meta::new(
+        Ast::VarPipe(
+            toks[0]
+                .item /* TODO: couldn't it be `ty` directly */
+                .item
+                .as_sym(),
+        ),
+        span,
+    )
+}
+
+fn type_decl(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let span = (toks[0].item.start, toks[0].item.end);
+    Meta::new(Ast::TypeDecl(toks[0].clone().into()), span)
+}
+
+fn elm_base(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks.into_iter().cloned().collect();
+    Meta::new(Ast::ElmBase(program), (start, end))
+}
+
+fn elm(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks.into_iter().cloned().collect();
+    Meta::new(Ast::Elm(program), (start, end))
+}
+
+fn prod(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks.into_iter().cloned().collect();
+    Meta::new(Ast::Prod(program), (start, end))
+}
+
+fn rule_pipe_repeater(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks.into_iter().cloned().collect();
+    Meta::new(Ast::RulePipeRepeater(program), (start, end))
+}
+
+fn rule_pipe(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks.into_iter().cloned().collect();
+    Meta::new(Ast::RulePipe(program), (start, end))
+}
+
+fn rule_decl(toks: &[Token<Meta<Ast>, Sym>]) -> Meta<Ast> {
+    let start = toks.first().unwrap().item.start;
+    let end = toks.last().unwrap().item.end;
+    let program = toks.into_iter().cloned().collect();
+    Meta::new(Ast::RuleDecl(program), (start, end))
 }
