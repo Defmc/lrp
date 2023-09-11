73e82c182d885691fc96d87adfa6b16a73907d0c
Defmc <defmc0101@gmail.com>
2023-09-10 22:20:04 -0400

    fix: apply clippy lints

--- wop/src/main.rs
+++ wop/src/main.rs
@@ -4,9 +4,8 @@ use logos::Logos;
 use lrp::{Clr, Parser, Token};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = std::env::args().skip(1).next().unwrap();
+    let path = std::env::args().nth(1).unwrap();
     let file = fs::read_to_string(path)?;
-
     let mut copy = Vec::new();
 
     let lexer = wop::Sym::lexer(&file).spanned().map(|(m, s)| {
@@ -18,11 +17,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut dfa = clr.simple_dfa(lexer);
 
-    if let Err(e) = dfa.trace(|st| /* println!("{:?}", st.stack_fmt()) */()) {
-        println!("FATAL: {e}");
-        println!("source:");
-        copy.into_iter().for_each(|a| print!("{a} "));
+    match dfa.trace(|_| ()) {
+        Err(e) => {
+            println!("FATAL: {e}");
+            println!("source:");
+            copy.into_iter().for_each(|a| print!("{a} "));
+            Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "impossible to parse").into())
+        }
+        Ok(p) => {
+            println!("{p:?}");
+            Ok(())
+        }
     }
-
-    Ok(())
 }
