70ada4f4ea24bb065f8a939d683b6c168631b6cc
Defmc <defmc0101@gmail.com>
2023-12-06 17:50:23 -0400

    feat!: bootstrap it

--- wop/src/lib.rs
+++ wop/src/lib.rs
@@ -107,47 +107,7 @@ pub enum Sym {
 }
 
 use lrp::{Dfa, Grammar, Parser, Slr, Token};
-use reduct_map::reduct_map;
-
-#[must_use]
-pub fn grammar() -> Grammar<Sym> {
-    #[allow(clippy::enum_glob_use)]
-    use Sym::*;
-
-    let rules = lrp::grammar_map! {
-    EntryPoint -> Program,
-
-    Program -> Program Alias Sc
-        |  Program Import Sc
-        |  Program RuleDecl Sc
-        |  Alias Sc
-        |  Import Sc
-        |  RuleDecl Sc,
-
-    IdentPath -> IdentPath PathAccess Ident
-        | Ident,
-
-    RuleDecl -> IdentPath TwoDots IdentPath Assign Rule,
-
-    Rule -> Rule Pipe RulePipe CodeBlock
-        | RulePipe CodeBlock,
-
-    RulePipe -> RulePipe RuleItem | RuleItem,
-
-    RuleItem -> IdentPath
-        | StrLit,
-
-    Import -> UseWord IdentPath PathAccess Glob
-        | UseWord IdentPath,
-
-    Alias -> AliasWord Ident IdentPath
-        | AliasWord StrLit IdentPath
-    };
-
-    Grammar::new(EntryPoint, rules, Eof)
-}
-
-pub mod reduct_map;
+pub mod out;
 
 pub fn lexer<'source>(
     source: &'source <Sym as Logos>::Source,
@@ -159,8 +119,8 @@ pub fn lexer<'source>(
 
 #[must_use]
 pub fn build_parser<I: Iterator<Item = Gramem>>(buffer: I) -> Dfa<Meta<Ast>, Sym, I> {
-    let parser = Slr::new(grammar());
-    parser.dfa(buffer, reduct_map())
+    let parser = Slr::new(out::grammar());
+    parser.dfa(buffer, out::reduct_map())
 }
 
 #[cfg(test)]
