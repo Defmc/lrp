92e692e6b223a63a1d809388034517e34fe507d6
Defmc <defmc0101@gmail.com>
2023-12-11 19:36:52 -0400

    refact: split `dump`'s implementation

--- wop/src/builder.rs
+++ wop/src/builder.rs
@@ -10,9 +10,7 @@ pub const REDUCTOR_LINTS: &str =
 #[derive(Debug, Default)]
 pub struct Builder {
     pub aliases: HashMap<String, SrcRef>,
-    pub rules: HashMap<String, Vec<Vec<SrcRef>>>,
-    pub reductors: HashMap<String, (SrcRef, Vec<SrcRef>)>,
-    pub item_aliases: HashMap<String, Vec<Vec<ItemAlias>>>,
+    pub rules: HashMap<String, RuleBuild>,
     pub imports: Vec<SrcRef>,
 }
 
@@ -25,6 +23,16 @@ pub struct ItemAlias {
     pub active: bool,
 }
 
+#[derive(Debug, Clone)]
+pub struct ProductionBuild {
+    pub production: Vec<SrcRef>,
+    pub codeblock: SrcRef,
+    /// return type of `self.codeblock`
+    pub ty: SrcRef,
+    pub aliases: Vec<ItemAlias>,
+}
+
+pub type RuleBuild = Vec<ProductionBuild>;
 impl ItemAlias {
     pub fn dump(&self, src: &str) -> String {
         let mut s = String::new();
@@ -75,9 +83,7 @@ impl Builder {
     }
 
     fn rule_decl(&mut self, rule_ident: SrcRef, rule_ty: SrcRef, rule: &[RulePipe], src: &str) {
-        let mut prods = Vec::new();
-        let mut reductors = Vec::new();
-        let mut item_aliases = Vec::new();
+        let mut rules = Vec::new();
         for prod in rule {
             assert_ne!(
                 prod.1.from_source(src),
@@ -85,123 +91,40 @@ impl Builder {
                 "missing code block for {:?}",
                 rule_ident.from_source(src)
             );
-            let (p, r, a) = self.get_complete_rule(prod, src);
-            prods.extend(p);
-            reductors.extend(r);
-            item_aliases.extend(a);
+            let base = ProductionBuild {
+                production: Vec::new(),
+                codeblock: prod.1,
+                ty: rule_ty,
+                aliases: Vec::new(),
+            };
+            rules.extend(self.get_production(&[base], &prod.0, src));
         }
 
         assert!(
             self.rules
-                .insert(rule_ident.from_source(src).to_string(), prods)
-                .is_none(),
-            "rule {} was already defined",
-            rule_ident.from_source(src)
-        );
-        assert!(
-            self.reductors
-                .insert(
-                    rule_ident.from_source(src).to_string(),
-                    (rule_ty, reductors)
-                )
-                .is_none(),
-            "rule {} was already defined",
-            rule_ident.from_source(src)
-        );
-        assert!(
-            self.item_aliases
-                .insert(rule_ident.from_source(src).to_string(), item_aliases)
+                .insert(rule_ident.from_source(src).to_string(), rules)
                 .is_none(),
             "rule {} was already defined",
             rule_ident.from_source(src)
         );
     }
 
-    fn get_complete_rule(
-        &self,
-        pipe: &RulePipe,
-        src: &str,
-    ) -> (Vec<Vec<SrcRef>>, Vec<SrcRef>, Vec<Vec<ItemAlias>>) {
-        fn push_all_prods(prods: &mut [Vec<SrcRef>], item: SrcRef) {
-            for prod in prods.iter_mut() {
-                prod.push(item);
-            }
-        }
-        let mut prods = vec![vec![]];
-        let mut item_aliases = vec![vec![]];
-        for (index, g) in pipe.0.iter().enumerate() {
-            let (g, optional, alias) = if let Ast::RuleItem(ref i, o, a) = g.item.item {
-                (i, o, a)
-            } else {
-                unreachable!()
-            };
-            let item_alias = ItemAlias {
-                alias: alias.unwrap_or(SrcRef::new(0, 0)),
-                optional: if optional { Some(true) } else { None },
-                index,
-                final_index: None,
-                active: alias.is_some(),
-            };
-            item_aliases
-                .iter_mut()
-                .for_each(|v| v.push(item_alias.clone()));
-
-            let clones = if optional { prods.clone() } else { Vec::new() };
-            match g.ty {
-                Sym::StrLit => push_all_prods(
-                    &mut prods,
-                    *self
-                        .aliases
-                        .get(g.item.span.from_source(src))
-                        .unwrap_or_else(|| {
-                            eprintln!(
-                                "using literal string for {} (maybe is missing an alias?)",
-                                g.item.span.from_source(src)
-                            );
-                            &g.item.span
-                        }),
-                ),
-                Sym::IdentPath => push_all_prods(
-                    &mut prods,
-                    *self
-                        .aliases
-                        .get(g.item.span.from_source(src))
-                        .unwrap_or(&g.item.span),
-                ),
-                Sym::Rule => match g.item.item {
-                    Ast::Rule(ref v) => {
-                        let mut new_prods = Vec::new();
-                        for prod in &prods {
-                            for var in v {
-                                for new_var in self.get_complete_rule(var, src).0 {
-                                    let mut prod = prod.clone();
-                                    prod.extend(new_var);
-                                    new_prods.push(prod);
-                                }
-                            }
-                        }
-                        prods = new_prods;
-                    }
-                    _ => unreachable!(),
-                },
-                _ => unreachable!("{g:?}"),
-            }
-            prods.extend(clones);
-            if optional {
-                let disables: Vec<_> = item_aliases
-                    .iter()
-                    .cloned()
-                    .map(|v| {
-                        let mut v = v;
-                        v.last_mut().unwrap().optional = Some(false);
-                        v
-                    })
-                    .collect();
-                item_aliases.extend(disables);
-            }
-        }
-        let prods_size_it = 0..prods.len();
-        (prods, prods_size_it.map(|_| pipe.1).collect(), item_aliases)
+    fn get_production(&self, origin: &[ProductionBuild], pipe: &[Gramem], src: &str) -> RuleBuild {
+        let mut out = Vec::new();
+        // Sym::StrLit => push_all_prods(
+        //     &mut prods,
+        //     *self
+        //         .aliases
+        //         .get(g.item.span.from_source(src))
+        //         .unwrap_or_else(|| {
+        //             eprintln!(
+        //                 "using literal string for {} (maybe is missing an alias?)",
+        //                 g.item.span.from_source(src)
+        //             );
+        //             &g.item.span
+        //         }),
+        // ),
+        out
     }
 
     fn token_decl(&mut self, tk: SrcRef, alias: SrcRef, src: &str) {
@@ -230,22 +153,29 @@ impl Builder {
         });
         writeln!(out, "\tlet mut map = lrp::RuleMap::new();").unwrap();
         for (r_name, impls) in &self.rules {
-            writeln!(
-                out,
-                "\tmap.insert({r_name}, lrp::grammar::Rule::new({r_name}, vec!["
-            )
-            .unwrap();
-            for imp in impls {
-                out.push_str("\t\tvec![");
-                for gramem in imp {
-                    write!(out, "{}, ", gramem.from_source(src)).unwrap();
-                }
-                out.push_str("],\n");
-            }
-            out.push_str("\n\t]));\n");
+            Self::write_rule(&mut out, r_name, &impls, src);
         }
         out.push_str("\n\tmap\n}");
         out
+    }
+
+    /// Writes the rule production
+    /// # Panics
+    /// Never.
+    pub fn write_rule(out: &mut String, name: &str, prods: &RuleBuild, src: &str) {
+        writeln!(
+            out,
+            "\tmap.insert({name}, lrp::grammar::Rule::new({name}, vec!["
+        )
+        .unwrap();
+        for prod in prods {
+            out.push_str("\t\tvec![");
+            for gramem in &prod.production {
+                write!(out, "{}, ", gramem.from_source(src)).unwrap();
+            }
+            out.push_str("],\n");
+        }
+        out.push_str("\n\t]));\n");
     }
 
     /// Dumps an expression that returns a `ReductMap`
@@ -258,29 +188,33 @@ impl Builder {
             writeln!(out, "\tuse {};", i.from_source(src)).unwrap();
         });
         writeln!(out, "\tlet mut map = lrp::ReductMap::new();\n").unwrap();
-        for (r_name, (ty, impls)) in &self.reductors {
-            let ty = ty.from_source(src);
-            for (i, imp) in impls.iter().enumerate() {
-                let mut item_aliases = String::new();
-                for alias in &self.item_aliases[r_name][i] {
-                    alias.write(&mut item_aliases, src);
-                }
-                writeln!(
-                    out,
-                    "\tfn lrp_wop_{r_name}_{i}(toks: &[Gramem]) -> lrp::Meta<{ty}> {{\n\t\tlrp::Meta::new({{ {item_aliases} {}}}, lrp::Span::new(toks[0].item.span.start, toks.last().unwrap().item.span.end))\n\t}}",
-                    imp.from_source(src).strip_prefix("->").unwrap().strip_suffix('%').unwrap()
-                )
-                .unwrap();
-            }
-            write!(out, "\tmap.insert({r_name}, vec![").unwrap();
-            for (i, _) in impls.iter().enumerate() {
-                write!(out, "lrp_wop_{r_name}_{i}, ").unwrap();
-            }
-            out.push_str("\t]);\n\n");
+        for (r_name, prods) in &self.rules {
+            Self::write_rule_reduction(&mut out, r_name, prods, src);
         }
 
         out.push_str("\tmap\n}");
         out
+    }
+
+    pub fn write_rule_reduction(out: &mut String, name: &str, prods: &RuleBuild, src: &str) {
+        let ty = prods[0].ty.from_source(src);
+        for (i, prod) in prods.iter().enumerate() {
+            let mut item_aliases = String::new();
+            for alias in &prod.aliases {
+                alias.write(&mut item_aliases, src).unwrap();
+            }
+            writeln!(
+                    out,
+                    "\tfn lrp_wop_{name}_{i}(toks: &[Gramem]) -> lrp::Meta<{ty}> {{\n\t\tlrp::Meta::new({{ {item_aliases} {}}}, lrp::Span::new(toks[0].item.span.start, toks.last().unwrap().item.span.end))\n\t}}",
+                    prod.codeblock.from_source(src).strip_prefix("->").unwrap().strip_suffix('%').unwrap()
+                )
+                .unwrap();
+        }
+        write!(out, "\tmap.insert({name}, vec![").unwrap();
+        for (i, _) in prods.iter().enumerate() {
+            write!(out, "lrp_wop_{name}_{i}, ").unwrap();
+        }
+        out.push_str("\t]);\n\n");
     }
 }
 
